68363b55f6447b854c893eb619e2092db2324acf
tuduun <gantulga01@allegheny.edu>
2024-07-24 18:56:08 -0400

    Fix: Increase chunk warning size

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Personal-Portfolio/',
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 KiB
+  },
   plugins: [react()]
 })
