220dcdd6e53cbe9f99042dbe462937c02415ab1c
tuduun <gantulga01@allegheny.edu>
2024-07-24 16:57:04 -0400

    Chore: Add footer and adjust tracing beam setting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => (
     </div>
     <div className="text-center font-poppins font-normal text-dimWhite text-xs sm:text-sm pb-4">
       <p>
-        Made by Tuguldur 2024
+        Made by Tuguldur &copy; 2024
       </p>
     </div>
   </footer>
