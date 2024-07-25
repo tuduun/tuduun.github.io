6cf97c57202ec8e5cc1d5f711eb5f44dcceaa212
tuduun <gantulga01@allegheny.edu>
2024-07-25 15:17:14 -0400

    Fix: Redirect to root in config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Personal-Portfolio/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 KiB
   },
